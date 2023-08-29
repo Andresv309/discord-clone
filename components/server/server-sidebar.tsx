69c3d91872d08f2b5233a67ce8de9c3ee5efab6a
Andresv309 <andresv309@gmail.com>
2023-08-29 17:44:50 -0500

    Create app sidebar

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,6 +1,8 @@
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
+import { ChannelType } from "@prisma/client"
 import { redirect } from "next/navigation"
+import { ServerHeader } from "./server-header"
 
 interface ServerSidebarProps {
   serverId: string
@@ -9,7 +11,7 @@ interface ServerSidebarProps {
 export const ServerSidebar = async ({
   serverId
 } : ServerSidebarProps) => {
-  const profile = currentProfile()
+  const profile = await currentProfile()
 
   if (!profile) {
     return redirect('/')
@@ -36,10 +38,24 @@ export const ServerSidebar = async ({
     }
   })
 
+  if (!server) {
+    return redirect('/')
+  }
+  
+  const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT)
+  const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO)
+  const videoChannels = server?.channels.filter((channel) => channel.type === ChannelType.VIDEO)
+
+  const members = server?.members.filter((member) => member.profileId !== profile.id)
+
+  const role = server.members.find((member) => member.profileId === profile.id)?.role
 
   return (
-    <div>
-      Server sidebar
+    <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
+      <ServerHeader
+        server={server}
+        role={role}
+      />
     </div>
   )
 }
