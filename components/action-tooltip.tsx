6bcba51c0d8a20018258816a3f93a8e65e962ae4
Andresv309 <andresv309@gmail.com>
2023-08-28 23:33:14 -0500

    Add modal server components

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip'
+
+interface ActionTooltipProps {
+  label: string
+  children: React.ReactNode
+  side?: 'top' | 'right' | 'bottom' | 'left'
+  align?: 'start' | 'center' | 'end'
+}
+
+
+export const ActionTooltip = ({
+  children,
+  label,
+  align,
+  side
+} : ActionTooltipProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>
+          {children}
+        </TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className='font-semibold text-sm capitalize'>
+            {label.toLocaleLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
