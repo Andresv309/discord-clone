07a9024c3f65a5042fb270d8cdd3bb98be5ce666
Andresv309 <andresv309@gmail.com>
2023-08-29 23:30:14 -0500

    Add modal providers

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react'
 
 import { CreateServerModal } from '@/components/modals/create-server-modal'
 import { InviteModal } from '@/components/modals/invite-modal'
+import { EditServerModal } from '@/components/modals/edit-server-modal'
+import { MembersModal } from '@/components/modals/members-modal'
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false)
@@ -20,6 +22,8 @@ export const ModalProvider = () => {
     <>
       <CreateServerModal />
       <InviteModal />
+      <EditServerModal />
+      <MembersModal />
     </>
   )
 }
