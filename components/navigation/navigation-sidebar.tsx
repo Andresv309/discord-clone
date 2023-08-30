a7301040e030fac26679255a46f45ef2e29ea784
Andresv309 <andresv309@gmail.com>
2023-08-29 23:28:50 -0500

    Fix bug when not retrieven servers correctly

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -18,7 +18,11 @@ export const NavigationSidebar = async () => {
 
   const servers = await db.server.findMany({
     where: {
-      profileId: profile.id
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
     }
   })
 
