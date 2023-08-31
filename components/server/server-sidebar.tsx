8b53a2233e90cc02b48cf47e2cde4bdd02fb1027
Andresv309 <andresv309@gmail.com>
2023-08-30 23:25:39 -0500

    Add search groups

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,11 +1,28 @@
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
-import { ChannelType } from "@prisma/client"
+import { ChannelType, MemberRole } from "@prisma/client"
 import { redirect } from "next/navigation"
 import { ServerHeader } from "./server-header"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { ServerSearch } from "./server-search"
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react"
+import { Separator } from "../ui/separator"
 
 interface ServerSidebarProps {
   serverId: string
+}
+
+const iconMap = {
+  [ChannelType.TEXT] : <Hash className="mr-2 w-4 h-4" />,
+  [ChannelType.AUDIO] : <Mic className="mr-2 w-4 h-4" />,
+  [ChannelType.VIDEO] : <Video className="mr-2 w-4 h-4" />
+}
+
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: <ShieldCheck className="mr-2 w-4 h-4 text-indigo-500" />,
+  [MemberRole.ADMIN]: <ShieldAlert className="mr-2 w-4 h-4 text-rose-500" />,
+
 }
 
 export const ServerSidebar = async ({
@@ -56,6 +73,58 @@ export const ServerSidebar = async ({
         server={server}
         role={role}
       />
+      <ScrollArea className="flex-1 px-3">
+        <div className="mt-2">
+          <ServerSearch
+            data={[
+              {
+                label: 'Text Channels',
+                type: 'channel',
+                data: textChannels.map(channel => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type]
+                }))
+              },
+              {
+                label: 'Voice Channels',
+                type: 'channel',
+                data: audioChannels.map(channel => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type]
+                }))
+              },
+              {
+                label: 'Video Channels',
+                type: 'channel',
+                data: videoChannels.map(channel => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type]
+                }))
+              },
+              {
+                label: 'Members',
+                type: 'member',
+                data: members.map(member => ({
+                  id: member.id,
+                  name: member.profile.name,
+                  icon: roleIconMap[member.role]
+                }))
+              },
+            ]}
+          />
+        </div>
+        <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2"/>
+        {
+          !!textChannels?.length && (
+            <div className="mb-2">
+
+            </div>
+          )
+        }
+      </ScrollArea>
     </div>
   )
 }
