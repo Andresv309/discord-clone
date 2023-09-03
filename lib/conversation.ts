7cdd5c8ba51e66eccb1fca9395fc49e173ec1418
Andresv309 <andresv309@gmail.com>
2023-09-03 15:36:26 -0500

    Add util function to bring conversation info

---
+++ lib/conversation.ts
@@ -0,0 +1,64 @@
+import { db } from '@/lib/db'
+
+export const getOrCreateConversation = async (memberOneId: string, memberTwoId: string) => {
+  let conversation = await findConversation(memberOneId, memberTwoId) || await findConversation(memberTwoId, memberOneId)
+
+  if (!conversation) {
+    conversation = await createNewConversation(memberOneId, memberTwoId)
+  }
+
+  return conversation
+}
+
+
+const findConversation = async (memberOneId: string, memberTwoId: string) => {
+  try {
+    return await db.conversation.findUnique({
+      where: {
+        memberOneId_memberTwoId : {
+          memberOneId,
+          memberTwoId
+        }
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true
+          }
+        },
+        memberTwo: {
+          include: {
+            profile: true
+          }
+        }
+      }
+    })
+  } catch {
+    return null
+  }
+}
+
+const createNewConversation = async (memberOneId: string, memberTwoId: string) => {
+  try {
+    return await db.conversation.create({
+      data: {
+        memberOneId,
+        memberTwoId
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true
+          }
+        },
+        memberTwo: {
+          include: {
+            profile: true
+          }
+        }
+      }
+    })
+  } catch {
+    return null
+  }
+}
