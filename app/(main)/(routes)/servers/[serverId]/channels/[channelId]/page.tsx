b96893e45f9dd551c33e122bf17ad68871384dcc
Andresv309 <andresv309@gmail.com>
2023-09-04 03:15:16 -0500

    Add routes for fetching messages

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,6 @@
 import { ChatHeader } from "@/components/chat/chat-header"
+import { ChatInput } from "@/components/chat/chat-input"
+import { ChatMessages } from "@/components/chat/chat-messages"
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
 import { redirectToSignIn } from "@clerk/nextjs"
@@ -43,6 +45,31 @@ const ChannelIdPage = async ({
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <ChatMessages
+        member={member}
+        name={channel.name}
+        chatId={channel.id}
+        type='channel'
+        apiUrl='/api/messages'
+        socketUrl='/api/socket/messages'
+        socketQuery={{
+          channelId: channel.id,
+          serverId: channel.serverId
+        }}
+        paramKey='channelId'
+        paramValue={channel.id}
+
+      />
+      <ChatInput
+        name={channel.name}
+        type='channel'
+        apiUrl='/api/socket/messages'
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId
+        }}
+      
       />
     </div>
   )
